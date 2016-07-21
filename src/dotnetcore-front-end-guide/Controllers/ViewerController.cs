3a9ca3b2946dc61b86ac8d4be53c5703e9b4f0df
Joao Antao <joaoantao@funda.nl>
2016-07-21 18:00:17 +0200

    Added Viewer controller and view

---
+++ src/dotnetcore-front-end-guide/Controllers/ViewerController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace dotnetcore_front_end_guide.Controllers
+{
+    public class ViewerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
