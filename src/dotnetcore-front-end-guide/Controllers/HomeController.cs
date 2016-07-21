bd70bb075a2f8b615e17406e2f53ad8fddfa13e6
Joao Antao <joaoantao@funda.nl>
2016-07-21 17:00:36 +0200

    Added .NET Core solution

---
+++ src/dotnetcore-front-end-guide/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace dotnetcore_front_end_guide.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "Home Page";
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
