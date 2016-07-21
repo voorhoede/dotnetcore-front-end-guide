deb2d1ac52ff1d51a59f087e659df9172c5082fa
Joao Antao <joaoantao@funda.nl>
2016-07-21 17:59:52 +0200

    Added Modules model, views and controller

---
+++ src/dotnetcore-front-end-guide/Controllers/ModulesController.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using dotnetcore_front_end_guide.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.AspNetCore.Mvc.Routing;
+
+namespace dotnetcore_front_end_guide.Controllers
+{
+    public class ModulesController : Controller
+    {
+        private readonly IUrlHelper _urlHelper;
+        private readonly string _modulesRootPath;
+
+        public ModulesController(IHostingEnvironment hostingEnvironment, IActionContextAccessor actionContextAccessor, IUrlHelperFactory urlHelperFactory)
+        {
+            _urlHelper = urlHelperFactory.GetUrlHelper(actionContextAccessor.ActionContext);
+            _modulesRootPath = Path.Combine(hostingEnvironment.ContentRootPath, "Views");
+        }
+
+        [Route("/modules/")]
+        public IActionResult Modules()
+        {
+            return Json(GetModules());
+        }
+
+        [Route("modules/{group}/{name}")]
+        public IActionResult Module(string group, string name)
+        {
+            return View("Module", $"~/Views/{group}/{name}/{name}.demo.cshtml");
+        }
+
+        [Route("modules/{group}/{name}/info")]
+        public IActionResult Info(string group, string name)
+        {
+            string readmePath = Path.Combine(_modulesRootPath, group, name, "README.md");
+            if (!System.IO.File.Exists(readmePath))
+            {
+                return Content(string.Empty);
+            }
+
+            using (var reader = new StreamReader(new FileStream(readmePath, FileMode.Open)))
+            {
+                return View("Info", CommonMark.CommonMarkConverter.Convert(reader.ReadToEnd()));
+            }
+        }
+
+        // TODO : Create a singleton-ish IOService.cs
+        private IEnumerable<Module> GetModules()
+        {
+            foreach (string moduleGroupDirectoryPath in Directory.GetDirectories(_modulesRootPath))
+            {
+                var moduleGroupDirectoryPathParts = moduleGroupDirectoryPath.Split(Path.DirectorySeparatorChar);
+                var moduleGroup = moduleGroupDirectoryPathParts.Last();
+
+                foreach (var moduleDirectoryPath in Directory.GetDirectories(moduleGroupDirectoryPath))
+                {
+                    var moduleDirectoryPathParts = moduleDirectoryPath.Split(Path.DirectorySeparatorChar);
+                    var moduleName = moduleDirectoryPathParts.Last();
+
+                    string[] demofiles = Directory.GetFiles(moduleDirectoryPath, $"{moduleName}.demo.cshtml", SearchOption.TopDirectoryOnly);
+
+                    if (!demofiles.Any())
+                    {
+                        yield break;
+                    }
+
+                    string[] readmeFiles = Directory.GetFiles(moduleDirectoryPath, "README.md", SearchOption.TopDirectoryOnly);
+                    yield return new Module()
+                    {
+                        group = moduleGroup,
+                        name = moduleName,
+                        info = readmeFiles.Any()
+                            ? new ModuleInfo { readme = _urlHelper.Action(nameof(Info), new { group = moduleGroup, name = moduleName }) }
+                            : null,
+
+                        url = _urlHelper.Action(nameof(Module), new { group = moduleGroup, name = moduleName })
+                    };
+                }
+            }
+        }
+    }
+}
